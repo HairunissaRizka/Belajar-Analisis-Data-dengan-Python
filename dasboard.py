1c50bae6e59ae7ffc1df54941729af60340b2549
HairunissaRizka <162313719+HairunissaRizka@users.noreply.github.com>
2024-03-10 14:18:04 +0700

    Update dasboard.py

--- dasboard.py
+++ dasboard.py
@@ -8,8 +8,8 @@ import seaborn as sns
 st.set_option('deprecation.showPyplotGlobalUse', False)
 
 # Load the data
-df_day = pd.read_csv("Belajar-Analisis-Data-dengan-Python/day.csv")
-df_hour = pd.read_csv("Belajar-Analisis-Data-dengan-Python/hour.csv")
+df_day = pd.read_csv("day.csv")
+df_hour = pd.read_csv("hour.csv")
 
 # Data Cleaning
 df_day["dteday"] = pd.to_datetime(df_day["dteday"])
